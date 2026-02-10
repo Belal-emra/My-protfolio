4078c00e91a1f0016d13c2066ec1125856e32559
Belal-emra <rizerelmasry@gmail.com>
2026-02-10 03:49:08 +0300

    Add initial CSS styles for the application

---
+++ css/style.c
@@ -0,0 +1,87 @@
+02"}
+body{
+margin:0;
+font-family:'Cairo',sans-serif;
+background:linear-gradient(135deg,#0a0a0a,#111);
+color:white;
+direction:rtl;
+scroll-behavior:smooth;
+}
+
+/* NAV /
+nav{
+display:flex;
+justify-content:space-between;
+padding:15px 40px;
+background:#000;
+box-shadow:0 0 20px #000;
+position:sticky;
+top:0;
+z-index:1000;
+}
+
+nav ul{
+display:flex;
+gap:20px;
+list-style:none;
+}
+
+nav a{
+color:white;
+text-decoration:none;
+font-weight:bold;
+transition:.3s;
+}
+
+nav a:hover{
+color:#00ffd5;
+}
+
+/ HEADER /
+header{
+text-align:center;
+padding:80px 20px;
+background:radial-gradient(circle,#111,#000);
+}
+
+/ CARDS /
+.card{
+margin:40px;
+padding:30px;
+background:#121212;
+border-radius:20px;
+box-shadow:0 0 25px #000;
+animation:fadeIn 1s ease;
+}
+
+.center{
+text-align:center;
+}
+
+/ TASBEEH /
+#counter{
+font-size:60px;
+margin:20px;
+color:#00ffd5;
+}
+
+/ BUTTON /
+button{
+padding:10px 20px;
+margin:5px;
+border:none;
+border-radius:10px;
+background:#00ffd5;
+cursor:pointer;
+font-weight:bold;
+}
+
+/ DARK MODE /
+.light{
+background:#f5f5f5;
+color:#000;
+}
+
+/ ANIMATION */
+@keyframes fadeIn{
+from{opacity:0;transform:tran
